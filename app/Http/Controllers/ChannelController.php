1f74cfa0a82d05cd13ab5a8567e78fc404f06d4c
ragingalpaca <lamune.cider@gmail.com>
2024-11-30 23:12:31 +0900

    検索機能の追加

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -41,14 +41,32 @@ class ChannelController extends Controller
      */
     public function show(string $id)
     {
+        $keyword = request('keyword');
+        $cmntChatType = request('type');
+
         // チャンネル情報を取得して表示
         $channelData = [
             'name' => 'チャンネル'.$id,
             'comments' => [
-                ['id' => 1, 'timestamp' => '10:00', 'message' => 'コメントA'],
-                ['id' => 2, 'timestamp' => '20:00', 'message' => 'コメントB'],
+                ['id' => 1, 'cmntChatType' => '1', 'timestamp' => '2024-10-10 10:00', 'message' => 'コメントA'],
+                ['id' => 2, 'cmntChatType' => '1', 'timestamp' => '2024-10-20 10:00', 'message' => 'コメントB'],
+                ['id' => 3, 'cmntChatType' => '2', 'timestamp' => '10:00', 'message' => 'チャットA'],
+                ['id' => 4, 'cmntChatType' => '2', 'timestamp' => '20:00', 'message' => 'チャットB'],
             ],
         ];
+
+        $channelData['comments'] = !$keyword
+        ? []
+        : array_filter(
+            $channelData['comments'],
+            function ($comment) use ($keyword, $cmntChatType) {
+                // タイプ一致かつメッセージが含まれるもののみtrue
+                return 
+                    ($comment['cmntChatType'] === $cmntChatType || $cmntChatType === '0')
+                    ? strpos($comment['message'], $keyword) !== false
+                    : false;
+            },
+        );
 
         return view('channels.show', compact('channelData'));
     }
