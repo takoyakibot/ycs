8bda8eb0d47d30be3ac5cc300f4fcd4441b248d4
ragingalpaca <lamune.cider@gmail.com>
2024-12-14 11:34:15 +0900

    profileでapikey更新

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
@@ -30,6 +32,24 @@ class ProfileController extends Controller
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
+        }
+
+        if ($request->user()->isDirty('api_key')) {
+            $old_user = User::where('id', $request->user()->id)->firstOrFail();
+            // 削除の場合はnull
+            if ($request->user()->api_key === '削除') {
+                $request->user()->api_key = null;
+            } elseif ($request->user()->api_key) {
+                // 入力されていれば暗号化して更新
+                error_log('request: ' . $request->user()->api_key);
+                error_log('auth: ' . $old_user->api_key);
+                if ($request->user()->api_key !== $old_user->api_key) {
+                    $request->user()->api_key = Crypt::encryptString($request->user()->api_key);
+                }
+            } else {
+                // 空の場合は古い値のまま変更しない
+                $request->user()->api_key = $old_user->api_key;
+            }
         }
 
         $request->user()->save();
