309a68a0b5bd75017d6fbffc743608b2e01c09b7
ragingalpaca <lamune.cider@gmail.com>
2024-12-25 22:52:37 +0900

    チャンネル管理 見た目

--- resources/views/manage/index.blade.php
+++ resources/views/manage/index.blade.php
@@ -1,38 +1,38 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold sm:text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('チャンネル管理') }}
         </h2>
     </x-slot>
 
-    <div class="px-6 py-12">
+    <div class="flex flex-col px-6 py-12 items-center">
 
         @if ($api_key_flg)
             <!-- チャンネル登録フォーム -->
-            <div class="p-2">
-                <form id="channelRegisterForm">
-                    <x-input-label for="handle" :value="__('handle')" class="mr-2" />
+            <form id="channelRegisterForm">
+                <div class="p-2 flex flex-col gap-2">
                     <div class="flex items-center gap-2">
+                        <x-input-label for="handle" :value="__('handle')" class="mr-2" />
                         <x-text-input id="handle" name="handle" type="text" class="mt-1 block w-[200px]" required autofocus
                             autocomplete="handle" placeholder="7777777" />
                         <x-primary-button id="registerButton" type="button" class="mt-1">登録</x-primary-button>
-                        <div class="gap-0">
-                            <span class="text-gray-500 mt-1">(
-                                https://youtube.com/@</span><span class="text-black font-bold mt-1">7777777</span><span
-                                class="text-gray-500 mt-1">
-                                )
-                            </span>
-                        </div>
                     </div>
-                </form>
-                <!-- エラーメッセージ表示 -->
-                <div id="errorMessage" class="text-red-500 mt-2"></div>
-            </div>
+                    <div class="gap-0">
+                        <span class="text-gray-500 mt-1">(handle ->
+                            https://youtube.com/@</span><span class="text-black font-bold mt-1">7777777</span><span
+                            class="text-gray-500 mt-1">
+                            )
+                        </span>
+                    </div>
+                    <!-- エラーメッセージ表示 -->
+                    <div id="errorMessage" class="text-red-500 mt-2"></div>
+                </div>
+            </form>
 
             <!-- チャンネル一覧 -->
             <div class="p-2">
-                <h2 class="text-gray-500">登録チャンネル</h2>
-                <div id="channels"></div>
+                <h3 class="text-lg text-gray-800 font-bold mb-2">登録チャンネル</h3>
+                <div id="channels" class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 w-[100%] max-w-5xl border shadow p-4 rounded-lg"></div>
             </div>
         @else
             <div>
