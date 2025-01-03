1dad723023f51fb2ab71d02983902b9b676c245a
ragingalpaca <lamune.cider@gmail.com>
2025-01-03 22:33:09 +0900

    いらんけど一応考慮

--- resources/views/channels/index.blade.php
+++ resources/views/channels/index.blade.php
@@ -12,19 +12,16 @@
 
     <div class="flex flex-col p-6 items-center">
         <div class="p-2">
-            <div x-data='{ channels: window.channels, selectedChannel: null }'
+            <div x-data='{ channels: window.channels }'
                 class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 w-[100%] max-w-5xl border shadow p-4 rounded-lg">
                 <template x-for="channel in channels" :key="channel.handle">
                     <a :href="'/channels/'+channel.handle">
                         <div class="flex items-center gap-4 border rounded-lg shadow-lg p-4 bg-white cursor-pointer hover:bg-gray-200">
-                            <img :src="escapeHTML(channel.thumbnail)" alt="アイコン" class="w-20 h-20 rounded-full" />
-                            <span class="text-lg font-bold" x-text="channel.title"></span>
+                            <img :src="escapeHTML(channel.thumbnail || '')" alt="アイコン" class="w-20 h-20 rounded-full" />
+                            <span class="text-lg font-bold" x-text="channel.title || '未設定'"></span>
                         </div>
                     </a>
                 </template>
-                <div x-show="selectedChannel" class="mt-4">
-                    <p>現在選択中のチャンネル： <span x-text="selectedChannel"></span></p>
-                </div>
             </div>
         </div>
     </div>
