883d94b27665d90e9393398b0327e94936528d0f
ragingalpaca <lamune.cider@gmail.com>
2025-01-11 01:18:15 +0900

    サーバーにあげるに当たって問題だったところを修正

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const isLocal = process.env.APP_ENV === 'local';
+
+    return {
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                    'resources/js/show.js',
+                    'resources/js/manage/archives.js',
+                    'resources/js/manage/channels.js',
+                ],
+                refresh: true,
+            }),
+        ],
+        build: {
+            outDir: 'public/build',
+        },
+        server: {
+            https: true,
+        }
+    };
 });
