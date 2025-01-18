2a4c805bab466432c220b84a5a6b387b9ae70bfb
ragingalpaca <lamune.cider@gmail.com>
2025-01-19 00:36:18 +0900

    ページネーション

---
+++ resources/views/components/pagination.blade.php
@@ -0,0 +1,14 @@
+<!-- pagination.blade.php -->
+<div id="paginationButtons">
+    <div class="pagination mt-4">
+        <!-- 前へボタン -->
+        <button class="pagination-button last pagination-button-disabled">
+            前へ
+        </button>
+
+        <!-- 次へボタン -->
+        <button class="pagination-button next">
+            次へ
+        </button>
+    </div>
+</div>
