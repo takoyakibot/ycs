18b465f69ed426d036c79de66525464bb18ab751
ragingalpaca <lamune.cider@gmail.com>
2025-01-19 02:30:05 +0900

    調整

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,14 +1,12 @@
 <!-- pagination.blade.php -->
-<div id="paginationButtons">
-    <div class="pagination mt-4">
-        <!-- 前へボタン -->
-        <button class="pagination-button last pagination-button-disabled">
-            前へ
-        </button>
+<div id="paginationButtons" class="flex gap-2 justify-center w-[100%]">
+    <!-- 前へボタン -->
+    <button class="pagination-button last">
+        <
+    </button>
 
-        <!-- 次へボタン -->
-        <button class="pagination-button next">
-            次へ
-        </button>
-    </div>
+    <!-- 次へボタン -->
+    <button class="pagination-button next">
+        >
+    </button>
 </div>
