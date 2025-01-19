a77d2d3d8b1eef933915d606664d7ef5be4c1279
ragingalpaca <lamune.cider@gmail.com>
2025-01-19 10:38:57 +0900

    指摘修正とページネーションボタンの調整

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,12 +1,12 @@
 <!-- pagination.blade.php -->
 <div id="paginationButtons" class="flex gap-2 justify-center w-[100%]">
     <!-- 前へボタン -->
-    <button class="pagination-button last">
+    <button class="pagination-button prev pagination-button-disabled" aria-label="Previous page">
         <
     </button>
 
     <!-- 次へボタン -->
-    <button class="pagination-button next">
+    <button class="pagination-button next" aria-label="Next page">
         >
     </button>
 </div>
